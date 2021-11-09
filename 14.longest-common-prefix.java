33bcccee286897ddbe780e933ca69f526c6851e5
BazzaDEV <bazarjani.kian@gmail.com>
2021-11-09 11:43:36 -0500

    Comment solution explanation

--- 14.longest-common-prefix.java
+++ 14.longest-common-prefix.java
@@ -8,11 +8,83 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
         
+        /* 
+
+        First, let's figure out what the maximum length of
+        the longest common prefix (LCP) could be.
+
+        This will always be the length of the smallest
+        string in the array of strings, 'strs'.
+
+        For example:
+
+        strs1 = { "Higher", "Highest" }
+        
+        Here, we can see that the LCP between the two
+        strings in 'strs1', "Higher" and "Highest", is 
+        "Highe".
+        
+        Now let's make a new array with the same strings
+        as in 'strs1', but with one addition:
+
+        strs2 = { "Higher", "Highest", "Hi" }
+
+        The first two words have a LCP, "Highe", 
+        just as before, but "Hi" does not share this
+        prefix with them because of its shorter length.
+
+        The LCP in 'strs2' is "Hi". Okay, one more:
+
+        strs3 = { "Higher", "Highest", "Hi", "H" }
+
+        Yet again, we can see that while some words share
+        prefixes, the longest common prefix among all the
+        strings is effectively bottlenecked by the shortest
+        string, that being "H".
+        
+        */
+
         int len = Integer.MAX_VALUE;
         
         for (String s : strs) {
             len = Math.min(s.length(), len);
         }
+
+        /*
+        
+        Each iteration of the outer (while) loop will allow us
+        to make the following decision:
+
+        "Is the ith character in the longest common prefix?"
+
+        We will check all characters up to, but not including,
+        the 'len'th character, where 'len' is the length of the
+        shortest string in 'strs', and thus the maximum length
+        of the LCP in 'strs'.
+
+        The inner (for) loop checks if the ith character is the
+        same for all strings in 'strs'. Two things can happen:
+
+        (1)  If it is, then we can add the ith character to
+             our longest common prefix and consider the next
+             character for the LCP.
+
+        (2)  If there are any two strings in 'strs' which do 
+             not have the same ith characters, then we cannot
+             add this character to the LCP ...
+        
+             ... and since we can no longer consider adding the
+             ith character to the LCP, we cannot consider adding
+             any more characters to the LCP. We can break out of
+             the inner & outer loops to end our search.
+        
+        At this point, we have finished iterating through both
+        loops. Either because all characters up to 'len' were
+        successfully considered for the LCP, or we broke early.
+
+        Nonetheless, we have found the LCP - problem solved!
+
+        */
         
         boolean flag = false;
         int i = 0;
