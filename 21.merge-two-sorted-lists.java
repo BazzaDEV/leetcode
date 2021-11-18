8a1fa5bca141cdf8b4d4f7d1f3046291b4c11b77
BazzaDEV <bazarjani.kian@gmail.com>
2021-11-17 20:03:41 -0500

    Make solution less garbage

--- 21.merge-two-sorted-lists.java
+++ 21.merge-two-sorted-lists.java
@@ -16,48 +16,24 @@
  * }
  */
 class Solution {
+
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 
         // We will use a 'dummy' node to represent the start of our merged list.
         ListNode dummy = new ListNode(0);
-        ListNode head = dummy;
+        ListNode curr = dummy;
 
-        // We are given the heads of two lists, l1 and l2.
-        // To build our merged sorted list, we need to compare the heads of the lists
-        // and make a decision: which node do we add to the merged list?
-        
-        // We need to add the smallest node to the merged list, remove that node from its
-        // original list, then repeat the process until both lists are empty.
+        while (l1 != null && l2 != null) {
+            curr.next = (l1.val < l2.val) ? l1 : l2;
 
-        while (l1 != null || l2 != null) {
-
-            if (l1 == null) {
-                head.next = l2;
-                break;
-
-            } else if (l2 == null) {
-                head.next = l1;
-                break;
-
-            } else if (l1.val < l2.val) {
-                ListNode temp = l1.next;
-                l1.next = null;
-                head.next = l1;
-                l1 = temp;
-
-            } else {
-                ListNode temp = l2.next;
-                l2.next = null;
-                head.next = l2;
-                l2 = temp;
-
-            }
-
-            head = head.next;
-
-            // print(dummy);
+            curr = curr.next;
+            if (l1.val < l2.val) l1 = l1.next;
+            else l2 = l2.next;
 
         }
+
+        if (l1 == null) curr.next = l2;
+        else if (l2 == null) curr.next = l1;
 
         return dummy.next;
         
