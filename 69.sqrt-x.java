ee96f23b677c9c13dcff02ccdc35cf5e6f74c3c8
BazzaDEV <bazarjani.kian@gmail.com>
2021-11-27 23:14:17 -0500

    Solve "Sqrt(x)" with Omar's method

--- 69.sqrt-x.java
+++ 69.sqrt-x.java
@@ -22,38 +22,20 @@ class Solution {
 
     public int mySqrt(int x) {
 
-        if (x == 0) return x;
+        if (x < 2) return x;
 
-        int left = 1, right = x, mid = left + (right - left)/2;
+        int left = 2, right = x/2, mid = left + (right-left)/2;
+        while (left <= right) {
+            mid = left + (right-left)/2;
 
-        while (left < right) {
-
-            mid = left + (right - left)/2;
-
-            // System.out.println("left " + left + " right " + right);
-            // System.out.println(mid + " " + x/mid);
-            // System.out.println();
-
-            if (mid <= x/mid && (mid+1) > x/(mid+1)) {
-                return mid;
-
-            } else if (mid > x/mid) {
-                right = mid;
-
+            if (mid > x/mid) { // same as mid * mid = x
+                right = mid - 1;
             } else {
                 left = mid + 1;
-
             }
-            
-
         }
 
-        // System.out.println("DONE!");
-        // System.out.println("left " + left + " right " + right);
-        // System.out.println(mid + " " + x/mid);
-        // System.out.println();
-
-        return left;
+        return right;
         
     }
 
